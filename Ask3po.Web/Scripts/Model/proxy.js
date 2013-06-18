02028a1ff9d6ef8b7b1b01d79ff63e396c9d05ba
Randolpho <randolpho@gmail.com>
2013-06-18 00:05:38 -0500

    Added some static data in preparation for the UI

---
+++ Ask3po.Web/Scripts/Model/proxy.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+
+var DecisionProxy = {
+
+    getDecision: function GetDecision(question, callback) {
+        var uri = "/decision?q=" + encodeURIComponent(question);
+        this.send(uri, "GET", null, callback);
+    },
+
+    // callback will be called with three parameters: json, status, response. 
+    //   json       the JSON Parsed response as an object
+    //   status     the http status code returned
+    //   response   the raw response text; json will be null if the response didn't parse as JSON. 
+    send: function Send(uri, method, data, callback) {
+        var request = new XMLHttpRequest();
+        request.open(method, uri, true);
+        request.responseType = "text";
+        request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+        request.onreadystatechange = function ResponseHandler() {
+            if (request.readyState != 4) return; // skip the other states; only interested in finished state
+            var json = null;
+            var response = request.responseText;
+            try {
+                json = JSON.parse(response);
+            }
+            catch (e) { /* do nothing */ }
+            callback(json, request.status, response);
+        }
+        if (data && typeof (data) !== "string") {
+            data = JSON.stringify(data);
+        }
+        request.send(data);
+    }
+};
---
+++ Ask3po.Web/Scripts/Model/static-data.js
@@ -0,0 +1,148 @@
+﻿var JukeboxTracks = [
+    { name: "20th Century-Fox Fanfare", url: "/static/music/1_01_Fanfare.mp3" },
+    { name: "Main Title/Rebel Blockade Runner", url: "/static/music/1_02_Main_Title.mp3" },
+    { name: "Imperial Attack", url: "/static/music/1_03_Imperial_Attack.mp3" },
+    { name: "Dune Sea of Tatooine/Jawa Sandcrawler", url: "/static/music/1_04_Dune_Sea.mp3" },
+    { name: "The Moisture Farm", url: "/static/music/1_05_Moisture_Farm.mp3" },
+    { name: "Hologram/Binary Sunset", url: "/static/music/1_06_Binary_Sunset.mp3" },
+    { name: "Landspeeder Search/Attack of the Sand People", url: "/static/music/1_07_Landspeeder_Search.mp3" },
+    { name: "Tales of a Jedi Knight/Learn About the Force", url: "/static/music/1_08_Learn_About_the_Force.mp3" },
+    { name: "Burning Homestead", url: "/static/music/1_09_Burning_Homestead.mp3" },
+    { name: "Mos Eisley Spaceport", url: "/static/music/1_10_Mos_Eisley.mp3" },
+    { name: "Cantina Band", url: "/static/music/1_11_Cantina.mp3" },
+    { name: "Cantina Band, No. 2", url: "/static/music/1_12_Cantina_2.mp3" },
+    { name: "Princess Leia's Theme", url: "/static/music/2_01_Leia_s_Theme.mp3" },
+    { name: "Millennium Falcon/Imperial Cruiser Pursuit", url: "/static/music/2_02_Millennium_Falcon.mp3" },
+    { name: "Destruction of Alderaan", url: "/static/music/2_03_Destruction_of_Alderaan.mp3" },
+    { name: "Death Star/The Stormtroopers", url: "/static/music/2_04_Death_Star.mp3" },
+    { name: "Wookiee Prisoner/Detention Block Ambush", url: "/static/music/2_05_Wookiee_Prisoner.mp3" },
+    { name: "Shootout in the Cell Bay/Dianoga", url: "/static/music/2_06_Shootout.mp3" },
+    { name: "The Trash Compactor", url: "/static/music/2_07_Trash_Compactor.mp3" },
+    { name: "Tractor Beam/Chasm Crossfire", url: "/static/music/2_08_Tractor_Beam.mp3" },
+    { name: "Ben Kenobi's Death/Tie Fighter Attack", url: "/static/music/2_09_Ben_Kenobi_s_Death.mp3" },
+    { name: "The Battle of Yavin", url: "/static/music/2_10_Battle_of_Yavin.mp3" },
+    { name: "Throne Room (End Titles)", url: "/static/music/2_11_Throne_Room.mp3" },
+    { name: "Binary Sunset (Alternative)", url: "/static/music/1_13_Binary_Sunset_Alternative.mp3" }
+];
+
+var FriendList = [
+    {
+        name: "Beaker",
+        img: "/static/img/beaker.jpg",
+        words: [
+            "Mee-me-meep-mee-me-me.",
+            "Mee me. Mee-mo-meeeee. Meeee!!"
+        ]
+    },
+    {
+        name: "Boomhauer",
+        img: "/static/img/boomhauer.png",
+        words: [
+            "Yeah man I tell ya what... Did one of them snipe hunts last night... man with them dang ol' sticks and bags and Whack! Whack! man, go Woooo-loo-loo-loo!",
+            "Dang ol she can't speak dang 'ol like we do man but dang 'ol tell you what man she speak the language of love man.",
+        ]
+    },
+    {
+        name: "Charley Brown's dad",
+        img: "/static/img/charley brown.jpg",
+        words: [
+            "Mwa wa mwa wa, wa wa mwaaaa.",
+            "Mwa mwa mwa. Wa mwa waaa mwa."
+        ]
+    },
+    {
+        name: "The Sweedish Chef",
+        img: "/static/img/chef.jpg",
+        words: [
+            "Gersh gurndy morn-dee burn-dee, burn-dee, flip-flip-flip-flip-flip-flip-flip-flip-flip.",
+            "Børk! Børk! Børk!"
+        ]
+    },
+    {
+        name: "Chewbacca",
+        img: "/static/img/chewbacca.png",
+        words: [
+            "Graaphrgh! Grwaaaawl, grph arawol",
+            "Grrrrrrrrrrrrr"
+        ]
+    },
+    {
+        name: "Cousin Itt",
+        img: "/static/img/cousin it.jpg",
+        words: [
+            "Uts sfoo dedidivuh PAH!",
+            "Fize fiz d'fide fuh dehdu d'foo hahie ehh!"
+        ]
+    },
+    {
+        name: "Captain Dathon",
+        img: "/static/img/dathon.jpg",
+        words: [
+            "Temba, his arms wide.",
+            "Darmok and Jalad at Tanagra.",
+            "Mirab, his sails unfurled.",
+            "Sokath - his eyes uncovered."
+        ]
+    },
+    {
+        name: "Gary",
+        img: "/static/img/gary.jpg",
+        words: ["Meow."]
+    },
+    {
+        name: "Hodor",
+        img: "/static/img/hodor.jpg",
+        words: [
+            "Hodor.",
+            "Hodor, hodor! Hodor."
+        ]
+    },
+    {
+        name: "Kenny",
+        img: "/static/img/kenny.png",
+        words: [
+            "Mmph mmmph mphfrph!",
+            "Mmphn frphha herrpha."
+        ]
+    },
+    {
+        name: "The Librarian",
+        img: "/static/img/librarian.jpg",
+        words: [
+            "Oook.",
+            "Oook ook eek ook. Oooook!"
+        ]
+    },
+    {
+        name: "Mushmouth",
+        img: "/static/img/mushmouth.jpg",
+        words: [
+            "Budeep a bubba da beep my lubba.",
+            "Dobe notbe libeben tobe boombe habe werbe. Hebe nebeberbe mabekebe sebens."
+        ]
+    },
+    {
+        name: "The Pyro",
+        img: "/static/img/pyro.png",
+        words: [
+            "Bwauuun,waanana!",
+            "Mmph mmmph mphfrph!",
+            "Mhnk nhya mgh mhph."
+        ]
+    },
+    {
+        name: "Q-Bert",
+        img: "/static/img/qbert.jpg",
+        words: ["@!*?@!"]
+    },
+    {
+        name: "R2D2",
+        img: "/static/img/r2d2.JPG",
+        words: ["Beep beep boop beep!", "Beepeepbeepwaaaaaaawebeep"]
+    },
+    {
+        name: "Schnitzel",
+        img: "/static/img/shnitzel.png",
+        words: ["Radda radda radda radda!", "Radda. Radda radda radda radda."]
+    }
+];
